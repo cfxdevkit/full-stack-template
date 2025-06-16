a83467f8a67c4f94733172c53d58bb2294d057e8
cfxdevkit <spcfxda+cdk@gmail.com>
2025-06-16 08:56:52 +0000

    Frontend and hardhat package update

---
+++ packages/frontend/src/generated-addresses.ts
@@ -0,0 +1,9 @@
+// This file is auto-generated. Do not edit it manually.
+export const contractAddresses = {
+  AccessControl: '0x5FbDB2315678afecb367f032d93F642f64180aa3' as const,
+  BasicCounter: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512' as const,
+  DataTypesDemo: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0' as const,
+  Lock: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9' as const,
+} as const
+
+export type ContractAddresses = typeof contractAddresses
